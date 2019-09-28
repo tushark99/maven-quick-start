ff9c5201f31c3a7ed83d6491ed296b11e7e1ab29
Tushar <sentida@gmail.com>
2019-09-28 21:03:58 +0530

    Adding dependency to commons lang 3

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,14 @@
 package clinic.programming.training;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
-    
+	
+	public int countWords(String words)
+	{
+			String[] separateWords = StringUtils.split(words,' ');
+			return (separateWords==null) ? 0 : separateWords.length;
+    }
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -10,5 +17,7 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	int count = app.countWords("I have four words");
+	System.out.println("Word Count : "+count);
     }
 }
